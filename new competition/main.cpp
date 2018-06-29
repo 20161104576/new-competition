90e2dfae671de4f0aa461a1cdeecdf7425e93b0f
20161104576 <fjh@czm.local>
2018-06-29 15:06:49 +0800

    1
    new

--- new competition/main.cpp
+++ new competition/main.cpp
@@ -6,45 +6,46 @@
 //  Copyright © 2018年 fjh. All rights reserved.
 //
 
-#include <iostream>
-using namespace std;
-struct racer{
-    char rac_name[20];//选手名字
-    char rac_sex;//选手性别
-    char rac_class[10];//选手班级
-    int rac_number[10];//选手电话号码
-    char rac_category;//节目类别
-    char rac_proname;//节目名称
-};
+# include<iostream>
+# include<iomanip>
+# define N  20   //评委的人数最多为20
+# define M  50   //参赛选手的人数最多为50
 
-struct rater{
-    char rat_name[20];//裁判名字
-    char rat_sex;//裁判性别
-    int rat_number[15];//裁判电话号码
-};
+using namespace std;
+
+float CountAthleteScore(int , float ave, int n, float s[], int m);
+void Sort(float a[], int n);
+void Print(float a[], int n,int bh[]);
+float b[M];
+int bh[M];
+
 int main()
 {
-    int i;
-    double score[5];
-    double max=-1;
-    double min=200;
-    double sum=0.0;
-    double x=0.0;
-    cin >>score[0]>>score[1]>>score[2]>>score[3]>>score[4];
-    for (i = 0; i< 5; i++)
-    {
-        sum = sum + score[i];
-        if (score[i] > max)
-        {
-            max = score[i];
-        }
-        if (score[i] < min)
-        {
-            min = score[i];
-        }
+    int i=0,n,m,r,t=0;
+    float s[N],a[M],ave=0.0,sum=0.0;
+    char name[20],sex[2];
+    cout<<"******欢迎使用评分系统******"<<endl;
+    cout<<"   请输入评委个数（>=3)：";
+    cin>>n;
+    cout<<"   请输入参赛选手个数(>=2)：";
+    cin>>m;
+    cout<<endl;
+    
+    
+    for(i=0;i<m;i++)
+    {cout<<"       选手参赛中     "<<endl;
+        cout<<"       编号为：";
+        cin>>bh[i];
+        a[r++]=CountAthleteScore(bh[i], ave, n,s, m);
     }
-    sum = sum - max - min;
-    x = sum / 3 ;
-    cout << "该选手的最后得分:" << x << endl;
+    
+    
+    
+    cout<<m<<"这位选手的平均成绩分别是："<<endl;
+    for(r=0;r<m;r++)
+        cout<<a[r]<<"  ";
+    cout<<endl;
+    Sort(a, m);
+    Print( a, n,bh);
     return 0;
 }
